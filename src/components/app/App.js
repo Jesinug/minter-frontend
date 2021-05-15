3e0ea9f1febfd68f781dd3c3b44b998b673881b8
Jesica Nugnez <jesica.nugnez@gmail.com>
2021-05-15 21:02:48 +0200

    First commit

---
+++ src/components/app/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Minter
+        </h1>
+        <a className="Quote">
+          "Everyone is an ignorant, but we all ignore different things"
+          <br></br>
+          <em>Albert Einstein</em>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
